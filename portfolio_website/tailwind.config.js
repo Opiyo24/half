0c386fe4fbb702d4431fd6d9e07f4a19835f6b91
Opiyo24 <gabbynyangor@gmail.com>
2024-08-05 09:49:15 +0300

    Coming alive

--- portfolio_website/tailwind.config.js
+++ portfolio_website/tailwind.config.js
@@ -2,12 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/*.html"],
   theme: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+
     extend: {
       colors: {
+        masterBg: '000',
         heroPrimary: '#0b206c',
-        heroText: '#2ff067',
+        heroText: '#08D665',
       }
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1rem',
+        lg: '1.5rem',
+        xl: '3rem',
+      },
+    }
   },
   plugins: [],
 }
