61c79bd6fe5b81ad04c8e62f964bac4f17858370
Opiyo24 <gabbynyangor@gmail.com>
2024-08-05 07:53:21 +0300

    Added sample images

--- portfolio_website/tailwind.config.js
+++ portfolio_website/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        heroPrimary: '#0b206c',
+        heroText: '#2ff067',
+      }
+    },
   },
   plugins: [],
 }
